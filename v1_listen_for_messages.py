2d11e7e98b2f4563724e98491bbb4f29eaf0766d
aalexander0422 <122483416+aalexander0422@users.noreply.github.com>
2023-09-17 23:18:38 -0500

    g

--- v1_listen_for_messages.py
+++ v1_listen_for_messages.py
@@ -1,5 +1,5 @@
 """
-ASHLEE ALEXANDER 8/14
+ASHLEE ALEXANDER
 Listens for messages on the queue.
 This process runs continuously. 
 
@@ -29,35 +29,35 @@ import pika, sys, os
 
 # define a main function to run the program
 def main():
-    # create a blocking connection to the RabbitMQ server
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host="looooocalhost")
-    )
+    """
+    
+    """
+
+     # create a blocking connection to the RabbitMQ server
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='LocalHost'))
     # use the connection to create a communication channel
     channel = connection.channel()
     # use the channel to declare a queue
-    channel.queue_declare(queue="hello")
-
+    channel.queue_declare(queue='hello')
     # define a callback function to be called when a message is received
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body.decode())
-
     # use the channel to consume messages from the queue
-    channel.basic_consume(queue="hello", on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
     # print a message to the console for the user
-    print(" [*] Waiting for messages. To exit press CTRL+C")
+    print(' [*] Waiting for messages. To exit press CTRL+C')
     # start consuming messages
     channel.start_consuming()
 
-
-# ---------------------------------------------------------------------------
-# If this is the script we are running, then call some functions and execute code!
-# ---------------------------------------------------------------------------
-if __name__ == "__main__":
+# Standard Python idiom to indicate main program entry point
+# This allows us to import this module and use its functions
+# without executing the code below.
+# If this is the program being run, then execute the code below
+if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print("Interrupted")
+        print('Interrupted')
         try:
             sys.exit(0)
         except SystemExit:
